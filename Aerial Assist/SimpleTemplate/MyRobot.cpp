548e28dc6c7bab4b9c7d102235e9fbb3f4ced9f6
team2840nicholas <nicholas.currault@pcds.org>
2014-01-09 17:53:12 -0700

    Testing optical encoders

--- Aerial Assist/SimpleTemplate/MyRobot.cpp
+++ Aerial Assist/SimpleTemplate/MyRobot.cpp
@@ -1,4 +1,6 @@
 #include "WPILib.h"
+#include "PID.h"
+#include "Timer.h"
 
 /**
  * This is a demo program showing the use of the RobotBase class.
@@ -8,29 +10,42 @@
  */ 
 class RobotDemo : public SimpleRobot
 {
-	RobotDrive myRobot; // robot drive system
+	Timer autonTimer;
 	CANJaguar frontLeft;
 	CANJaguar frontRight;
 	CANJaguar backRight;
 	CANJaguar backLeft;
+	RobotDrive myRobot; // robot drive system
+	
 	Joystick stick; // only joystick
+	
+	PID driveLeftPID;
+	PID driveRightPID;
 
 public:
 	RobotDemo(void):
 		
+		stick(1),
 		frontLeft(14),
 		backLeft(15),
 		frontRight(11),
 		backRight(10),
-		myRobot(frontLeft, backLeft, frontRight, backRight),	// these must be initialized in the same order
-		stick(1)		// as they are declared above.
-	{
-		myRobot.SetExpiration(0.1);
+		myRobot(frontLeft, backLeft, frontRight, backRight),
+		driveLeftPID(0,0,0,100,100),
+		driveRightPID(0,0,0,100,100)
 		
-		backLeft.ConfigEncoderCodesPerRev(250);
-		backLeft.SetSpeedReference(CANJaguar::kSpeedRef_Encoder);
-		backRight.ConfigEncoderCodesPerRev(250);
-		backRight.SetSpeedReference(CANJaguar::kSpeedRef_Encoder);
+	{
+		/*
+		backLeft.SetSpeedReference(CANJaguar::kSpeedRef_QuadEncoder);
+		backRight.SetSpeedReference(CANJaguar::kSpeedRef_QuadEncoder);
+		*/
+		backRight.SetPositionReference(CANJaguar::kPosRef_QuadEncoder);
+		backLeft.SetPositionReference(CANJaguar::kPosRef_QuadEncoder);
+		
+		backLeft.ConfigEncoderCodesPerRev(360);
+		backRight.ConfigEncoderCodesPerRev(360);
+		
+		myRobot.SetExpiration(0.1);
 	}
 
 	/**
@@ -38,10 +53,18 @@ public:
 	 */
 	void Autonomous(void)
 	{
-		myRobot.SetSafetyEnabled(false);
-		myRobot.Drive(-0.5, 0.0); 	// drive forwards half speed
-		Wait(2.0); 				//    for 2 seconds
-		myRobot.Drive(0.0, 0.0); 	// stop robot
+		autonTimer.Start();
+		// bool isHot = true;
+		
+		double distanceTravelled = 0;
+		while (autonTimer.Get() < 5)
+		{
+			myRobot.Drive(-0.5,0.0);
+			distanceTravelled = 18.85 * ((backRight.GetPosition()+backLeft.GetPosition())/2.0);
+			SmartDashboard::PutNumber("Distance",distanceTravelled); 
+		}
+		myRobot.Drive(0.0,0.0);
+		
 	}
 
 	/**
@@ -53,6 +76,13 @@ public:
 		while (IsOperatorControl())
 		{
 			myRobot.ArcadeDrive(stick); // drive with arcade style (use right stick)
+			
+			distanceTravelled = 18.85 * ((backRight.GetPosition()+backLeft.GetPosition())/2.0);
+			SmartDashboard::PutNumber("Distance",distanceTravelled);
+			SmartDashboard::PutNumber("Distance R",distanceTravelled);
+			SmartDashboard::PutNumber("Distance L",distanceTravelled);
+			
+			
 			Wait(0.005);				// wait for a motor update time
 		}
 	}
